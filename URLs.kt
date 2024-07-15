eb1810e7f31f24f34eb0d51c386babe4f4dd8a9a
RouthuAnjani <130381213+RouthuAnjani@users.noreply.github.com>
2024-07-15 14:59:42 +0530

    Add files via upload

---
+++ URLs.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinwebservices
+
+object URLs {
+    private val ROOT_URL = "http://192.168.1.35/androidphpmysql/registrationapi.php?apicall="
+    val URL_REGISTER = ROOT_URL + "signup"
+    val URL_LOGIN = ROOT_URL + "login"
+}
---
+++ User.kt
@@ -0,0 +1,3 @@
+package com.example.kotlinwebservices
+
+class User(var id: Int, var name: String?, var email: String?, var gender: String?)
